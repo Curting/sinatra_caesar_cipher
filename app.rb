45ad99f0eb5f60f9d30150f2a64a2af15916bbca
Oliver <oc@hejverden.dk>
2017-11-30 13:50:44 +0100

    ready for Heroku deploy

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/reloader' if development?
 
 require_relative 'lib/caesar_cipher'
 
 get '/' do
-  erb :index
+  message = params["message"]
+
+  unless params["shift"].nil?
+    shift = params["shift"].to_i
+  end
+
+  unless message.nil? && shift.nil?
+    encrypted = caesar_cipher(message, shift)
+  end
+
+  erb :index, :locals => { :encrypted => encrypted }
 end
